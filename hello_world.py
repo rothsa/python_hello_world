2a18e16e9cf4d5e14befa3939c49ea31daca5a3a
TheAtomicOption <you@example.com>
2014-03-12 22:17:52 -0700

    fix input to raw input

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 print "hello world!"
-name = input('What is your name?')
+name = raw_input('What is your name?')
 print (type(name))
 print("hello " + name)
 print("nice to meetcha")
 age = input("I'm a brand new program zero years old. How old are you?")
-print("wow that means you are" +age+ "years older than me!")
+print("wow that means you are " + str(age) + " years older than me!")
