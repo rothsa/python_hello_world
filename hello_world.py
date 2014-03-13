c41a01f35d8a9113c092cd913140e5150629afe4
TheAtomicOption <you@example.com>
2014-03-12 22:11:27 -0700

    age question

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,6 @@ print "hello world!"
 name = input('What is your name?')
 print (type(name))
 print("hello " + name)
+print("nice to meetcha")
+age = input("I'm a brand new program zero years old. How old are you?")
+print("wow that means you are" +age+ "years older than me!")
